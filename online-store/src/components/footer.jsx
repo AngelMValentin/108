eb1ec00865a90a9a30d445d8ad3ec1589f6ff86f
AngelMValentin <angelvalentinusmc@gmail.com>
2024-12-21 15:08:49 -0800

    class 1

---
+++ online-store/src/components/footer.jsx
@@ -0,0 +1,19 @@
+import './styles/footer.css';
+
+function footer() {
+    return (
+        <div>
+            <nav>
+                <div class="footer-menu">
+                    <a href="">log in</a>
+                    <a href="">search</a>
+                    <a href="">subscribe</a>
+                    <a href="">menu</a>
+
+                </div>
+            </nav>
+        </div>
+    );
+}
+
+export default footer;
---
+++ online-store/src/components/navbar.jsx
@@ -0,0 +1,19 @@
+import './styles/navbar.css';
+
+function Navbar() {
+    return (
+        <div>
+            <div>
+                <div class="navbar-menu">
+                    <a href="">log in</a>
+                    <a href="">search</a>
+                    <a href="">subscribe</a>
+                    <a href="">menu</a>
+
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default Navbar;
