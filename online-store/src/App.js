9e7fc30f65786267fbd7200567e866b8ece308d7
AngelMValentin <angelvalentinusmc@gmail.com>
2025-01-04 14:37:45 -0800

    class 3

--- online-store/src/App.js
+++ online-store/src/App.js
@@ -2,23 +2,30 @@ import logo from './logo.svg';
 import './App.css';
 import Navbar from './components/navbar';
 import Footer from './components/footer';
-import Catalog from './pagez/catalog';
-import Product from './components/product';
+import Catalog from './pages/catalog';
+import Home from './pages/home.jsx';
+import About from './pages/about.jsx';
 
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.min.js';
 
 function App() {
   return (
+    <BrowserRouter>
     <div className="App">
       <Navbar></Navbar>
       
-      <Catalog></Catalog>
-      <Product></Product>
-      <Product></Product>
-      <Product></Product>
-      <Product></Product>
-      <Product></Product>
+      <Routes>
+        <Route path='/' element={<Home></Home>}></Route>
+        <Route path='/catalog' element={<Catalog></Catalog>}></Route>
+        <Route path='/about' element={<About></About>}></Route>
+      </Routes>
+
       <Footer></Footer>
     </div>
+    </BrowserRouter>
   );
 }
 
---
+++ online-store/src/pages/catalog.jsx
@@ -0,0 +1,17 @@
+import "./styles/catalog.css";
+import Product from '../components/product.jsx';
+
+function Catalog() {
+    return (
+        <div className="catalog">
+            <h1>Check out our amazing catalog!</h1>
+            <Product></Product>
+            <Product></Product>
+            <Product></Product>
+            <Product></Product>
+            <Product></Product>
+        </div>
+    )
+}
+
+export default Catalog;
