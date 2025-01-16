bacaeb9e308778dcfcb96ffc091c483b7794a399
AngelMValentin <angelvalentinusmc@gmail.com>
2025-01-15 21:35:05 -0800

    touch up

--- online-store/src/components/navbar.jsx
+++ online-store/src/components/navbar.jsx
@@ -14,7 +14,9 @@ function Navbar() {
     return (
         <nav className="navbar navbar-expand-lg">
             <div className="container-fluid">
-                <img className='companyLogo' src={Logo}></img>
+                <Link to='/'>
+                    <img className='companyLogo' src={Logo} href='#'></img>
+                </Link>
                 <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                     <span className="navbar-toggler-icon"></span>
                 </button>
