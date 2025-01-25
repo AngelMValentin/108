c65b08f2615b22329fc96028961e894f536ef6ee
AngelMValentin <angelvalentinusmc@gmail.com>
2025-01-25 13:46:25 -0800

    final report

--- online-store/src/pages/about.jsx
+++ online-store/src/pages/about.jsx
@@ -2,9 +2,49 @@ import './styles/about.css';
 
 function About() {
     return (
-        <div className='about'>
-            <h1>Welcome to the About Page</h1>
+        <div className='teamCardsContainer'>
+            <div className='aboutHeading'>
+                <h1>Meet our Amazing team!</h1>
+            </div>
+
+            <div className='teamMemberContainer'>
+                <div className='Container1'>    
+                </div>
+
+                <div className='memberInfo'>
+                    <h3 className='memberName'>Name</h3>
+                    <label>quote 1</label>
+                    <button className='addBtn btn btn-primary btn-sm' >View bio</button>
+                </div>
+            </div>
+
+
+
+            <div className='teamMemberContainer'>
+                <div className='Container2'>
+                </div>
+
+                <div className='memberInfo'>
+                    <h3 className='memberName'>Name</h3>
+                    <label>quote 2</label>
+                    <button className='addBtn btn btn-primary btn-sm' >View bio</button>
+                </div>
+            </div>
+
+
+
+            <div className='teamMemberContainer'>
+                <div className='Container3'>
+                </div>
+
+                <div className='memberInfo'>
+                    <h3 className='memberName'>Name</h3>
+                    <label>quote 3</label>
+                    <button className='addBtn btn btn-primary btn-sm' >View bio</button>
+                </div>
+            </div>
         </div>
+
     )
 }
 
